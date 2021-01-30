349ca0d69e822cb8b4677d8d813204e076a9a447
krownsi <alexander.gruenwald@mail.mcgill.ca>
2021-01-30 23:35:33 +0100

    vue setup

---
+++ hello-world/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+      <div>
+      <b-jumbotron header="BootstrapVue" lead="Bootstrap v4 Components for Vue.js 2">
+        <p>For more information visit website</p>
+        <b-button variant="primary" href="#">More Info</b-button>
+      </b-jumbotron>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
