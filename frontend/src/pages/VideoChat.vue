c7bb9061531ba0215d8bbfdf2275c73da04893e3
kdassharma <kaustav.dassharma@mail.mcgill.ca>
2021-01-31 18:54:55 -0500

    Added basic buttons for chat page

--- frontend/src/pages/VideoChat.vue
+++ frontend/src/pages/VideoChat.vue
@@ -1,15 +1,26 @@
 <template>
-  <div class="bg-primary vh-100 vw-100">
-    <b-container>
-        <b-row id="videos">
-            <b-col>
-                <video id="localVideo" muted autoplay playsinline></video>
-            </b-col>
-            <b-col>
-                <video id="remoteVideo" autoplay playsinline></video>
-            </b-col>
-        </b-row>
+  <div>
+    <b-container align="center">
+      <h1>Welcome to FirebaseRTC!</h1>
     </b-container>
+    <div id="buttons">
+    <button class="mdc-button mdc-button--raised" id="cameraBtn">
+        <i class="material-icons mdc-button__icon" aria-hidden="true"></i>
+        <span class="mdc-button__label">Open camera & microphone</span>
+    </button>
+    <button class="mdc-button mdc-button--raised" disabled id="createBtn">
+        <i class="material-icons mdc-button__icon" aria-hidden="true"></i>
+        <span class="mdc-button__label">Create room</span>
+    </button>
+    <button class="mdc-button mdc-button--raised" disabled id="joinBtn">
+        <i class="material-icons mdc-button__icon" aria-hidden="true"></i>
+        <span class="mdc-button__label">Join room</span>
+    </button>
+    <button class="mdc-button mdc-button--raised" disabled id="hangupBtn">
+        <i class="material-icons mdc-button__icon" aria-hidden="true"></i>
+        <span class="mdc-button__label">Hangup</span>
+    </button>
+</div>
   </div>
 </template>
 
@@ -27,3 +38,5 @@ export default {
   
 <style>
 </style>
+
+
