ab5400b7dad7ba61647f3353779f2e4c580f8d60
rbehal <rahulbehal01@hotmail.com>
2021-02-04 23:35:46 -0500

    Made login page mobile friendly + fixed bugs

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,25 +1,19 @@
 <template>
   <div>
     <b-container class="m-0 p-0 mw-100">
-      <b-row class="vh-100">
-        <b-col class="col-7 d-flex align-items-center">
-          <b-row class="w-100" align-v="center">
+      <b-row no-gutters class="vh-100">
+        <b-col class="col-md-5 col-12 d-flex align-items-center">
+          <b-row no-gutters class="w-100" align-v="center">
             <b-col class="px-0">
               <b-jumbotron
                 bg-variant="primary"
-                class="d-flex flex-column m-0 vh-100 justify-content-center align-items-center"
+                class="d-flex flex-column vh-100 justify-content-center align-items-center"
               >
                 <b-row class="justify-content-center w-75">
                   <b-col flex class="justify-content-center">
                     <template>
                       <div>
                         <b-row class="text-center my-3" align-v="center">
-                          <b-col class="px-0 col-1">
-                            <b-icon
-                              variant="danger"
-                              icon="circle-fill"
-                            ></b-icon>
-                          </b-col>
                           <b-col class="px-1">
                             <b-form-input
                               v-model="email"
@@ -28,12 +22,6 @@
                           </b-col>
                         </b-row>
                         <b-row class="text-center my-3" align-v="center">
-                          <b-col class="px-0 col-1">
-                            <b-icon
-                              variant="danger"
-                              icon="circle-fill"
-                            ></b-icon>
-                          </b-col>
                           <b-col class="px-1">
                             <b-form-input
                               v-model="password"
@@ -46,21 +34,35 @@
                     </template>
                   </b-col>
                 </b-row>
-                <b-row class="justify-content-center w-75">
-                  <b-col class="text-center">
-                    <b-button pill variant="warning" v-on:click="createAccount">
-                      Create Account</b-button>
+                <b-row no-gutters class="justify-content-center w-75 h-5">
+                  <b-col class="text-center mx-2 my-1">
+                    <b-button
+                      class="h-100"
+                      block
+                      pill
+                      variant="warning"
+                      v-on:click="createAccount"
+                    >
+                      Create Account</b-button
+                    >
                   </b-col>
-                  <b-col class="text-center">
-                    <b-button pill variant="warning" v-on:click="login">
-                      Login</b-button>
+                  <b-col class="text-center mx-2 my-1">
+                    <b-button
+                      class="h-100"
+                      block
+                      pill
+                      variant="warning"
+                      v-on:click="login"
+                    >
+                      Login</b-button
+                    >
                   </b-col>
                 </b-row>
               </b-jumbotron>
             </b-col>
           </b-row>
         </b-col>
-        <b-col class="col-5 d-flex align-items-center">
+        <b-col class="col-md-7 d-flex align-items-center px-4">
           <b-img
             fluid-grow
             alt="McGill logo"
@@ -73,32 +75,34 @@
 </template>
 
 <script>
-import {auth} from "../firebase";
+import { auth } from "../firebase";
 
 export default {
   name: "Home",
-  data: function () {
+  data: function() {
     return {
       email: "",
       password: "",
     };
   },
   methods: {
-    createAccount: function () {
-      auth.createUserWithEmailAndPassword(this.email, this.password)
-      .then((userCredential) => {
-        this.$route.params.sharedData = userCredential;
-        this.$router.push({ name: 'VideoChat' });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    },
-    login: function () {
-      auth.signInWithEmailAndPassword(this.email, this.password)
+    createAccount: function() {
+      auth
+        .createUserWithEmailAndPassword(this.email, this.password)
         .then((userCredential) => {
           this.$route.params.sharedData = userCredential;
-          this.$router.push({ name: 'VideoChat' }); 
+          this.$router.push({ name: "VideoChat" });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    login: function() {
+      auth
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then((userCredential) => {
+          this.$route.params.sharedData = userCredential;
+          this.$router.push({ name: "VideoChat" });
         })
         .catch((error) => {
           console.log(error);
@@ -107,6 +111,5 @@ export default {
   },
 };
 </script>
-  
-<style>
-</style>
+
+<style></style>
